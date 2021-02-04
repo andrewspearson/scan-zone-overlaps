818c34d8050d93497f8ff83b6823938d2e0fa935
Andrew Pearson <76594022+andrewspearson@users.noreply.github.com>
2021-02-04 08:09:02 -0500

    Update scan-zone-overlaps.py

--- scan-zone-overlaps.py
+++ scan-zone-overlaps.py
@@ -1,6 +1,32 @@
+import argparse
 import getpass
-from tenable.sc import TenableSC
 import ipaddress
+import urllib.request
+from urllib.error import HTTPError
+import ssl
+import json
+
+
+def api_call(method, host, endpoint, headers=None, data=None, proxy=None, verify=True):
+    request = urllib.request.Request('https://' + host + endpoint)
+    request.method = method
+    request.add_header('Accept', 'application/json')
+    context = ''
+    if headers:
+        for key, value in headers.items():
+            request.add_header(key, value)
+    if data:
+        request.data = json.dumps(data).encode()
+    if proxy:
+        request.set_proxy(proxy, 'https')
+    if verify is False:
+        context = ssl._create_unverified_context()
+    try:
+        response = urllib.request.urlopen(request, context=context)
+        return response
+    except HTTPError as error:
+        print('\nERROR: HTTP ' + str(error.code) + ' - https://' + host + endpoint)
+        print(error.reason)
 
 
 def normalize(ip_range):
@@ -35,29 +61,96 @@ def default_scan_zone_check(scan_zone):
         return True
 
 
-# Login
-address = input('tenable.sc IP or hostname: ')
+# Gather arguments. Proxy and ssl verification arguments are not required.
+arguments = argparse.ArgumentParser(description='Tool to uncover overlaps in Tenable.SC Scan Zones')
+arguments.add_argument(
+    '-p',
+    '--proxy',
+    metavar='127.0.0.1:8080',
+    default='',
+    dest='proxy',
+    help='HTTPS proxy'
+)
+arguments.add_argument(
+    '-i',
+    '--insecure',
+    action='store_false',
+    dest='verify',
+    help='Disable SSL verification'
+)
+arguments = arguments.parse_args()
+
+host = input('tenable.sc IP or hostname: ')
 username = input('Username: ')
 password = getpass.getpass()
-sc = TenableSC(address)
-sc.login(username, password)
+proxy = arguments.proxy
+verify = arguments.verify
+
+# Login
+response = api_call(
+    method='GET',
+    host=host,
+    endpoint='/rest/system',
+    proxy=proxy,
+    verify=verify
+)
+cookie = response.headers['Set-Cookie'].split(';', 1)[0]
+response = api_call(
+    method='POST',
+    host=host,
+    endpoint='/rest/token',
+    headers={
+        "Cookie": cookie
+    },
+    data={
+        "username": username,
+        "password": password
+    },
+    proxy=proxy,
+    verify=verify
+)
+token = json.load(response)['response']['token']
+cookie = response.headers['Set-Cookie'].split(';', 1)[0]
+
+current_user = api_call(
+    method='GET',
+    host=host,
+    endpoint='/rest/currentUser',
+    headers={
+        "X-SecurityCenter": token,
+        "Cookie": cookie
+    },
+    proxy=proxy,
+    verify=verify
+)
 
 # Check if this is an Application Administrator user
-current_user = sc.current.user()
-if current_user['organization']['id'] != 0 or current_user['role']['id'] != '1':
+current_user = json.load(current_user)
+if current_user['response']['organization']['id'] != 0 or current_user['response']['role']['id'] != '1':
     print('\nWARNING: You are logging in with an account that is not a member of the \'Tenable.sc Administration\' '
           'organization and/or is not assigned the \'Administrator\' role. This script will continue to run, however '
           'results may be incomplete.')
 
 # Pull Scan Zones
-scan_zones = sc.scan_zones.list()
+scan_zones = api_call(
+    method='GET',
+    host=host,
+    endpoint='/rest/zone',
+    headers={
+        "X-SecurityCenter": token,
+        "Cookie": cookie
+    },
+    proxy=proxy,
+    verify=verify
+)
+scan_zones = json.load(scan_zones)
 
 # Normalize ipList values into IPv4Network objects
-for scan_zone in scan_zones:
+for scan_zone in scan_zones['response']:
     scan_zone['ipList'] = normalize(scan_zone['ipList'])
 
 # Find and report overlaps
-for scan_zone1 in scan_zones:
+for scan_zone1 in scan_zones['response']:
     if not default_scan_zone_check(scan_zone1):
         scan_zone1_id = scan_zone1['id']
         alerts = []
@@ -66,7 +159,7 @@ for scan_zone1 in scan_zones:
         print('---------------------------------------------')
         for item1 in scan_zone1['ipList']:
             for network1 in item1['networks']:
-                for scan_zone2 in scan_zones:
+                for scan_zone2 in scan_zones['response']:
                     if not default_scan_zone_check(scan_zone2):
                         if scan_zone1_id != scan_zone2['id']:
                             for item2 in scan_zone2['ipList']:
@@ -82,4 +175,14 @@ for scan_zone1 in scan_zones:
             print(alert)
 
 # Logout
-sc.logout()
+api_call(
+    method='DELETE',
+    host=host,
+    endpoint='/rest/token',
+    headers={
+        "X-SecurityCenter": token,
+        "Cookie": cookie
+    },
+    proxy=proxy,
+    verify=verify
+)
